e44d36a3bbf1c6fb8b597b34025a1923dfd418a1
lilzooty <nickkroop@gmail.com>
2024-10-19 22:22:01 -0600

    updated stuff

---
+++ js/myScript.js
@@ -0,0 +1,17 @@
+
+function goHomePage() {
+    window.location.href = "index.html"
+}
+
+function goProjectPage() {
+    window.location.href = "projectPage.html"
+}
+
+function goResumePage(){ 
+window.location.href = "resumePage.html"
+}
+
+function goContactPage() {
+    window.location.href = "contactPage.html"
+}
+    
